2307e6e057306b88f986bd1e6aece58e296a213d
gabriel850129 <gabriel850129@icloud.com>
2024-04-16 23:07:31 -0400

    Initial Commit

---
+++ LocationExample/LocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationExampleApp.swift
+//  LocationExample
+//
+//  Created by Gabriel Estévez López on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
