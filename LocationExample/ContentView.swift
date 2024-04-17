cf3d86f56591fe158c76858d8833a667d87698d0
Wilder Lopez <wlopezelias@gmail.com>
2024-04-17 13:14:56 +0200

    Updated deprecated Map constructor Added new key for location "NSLocationAlwaysUsageDescription"

--- LocationExample/ContentView.swift
+++ LocationExample/ContentView.swift
@@ -11,11 +11,17 @@ import MapKit
 
 struct ContentView: View {
     
-    @StateObject var manager = LocationManager()
+//    @StateObject var manager = LocationManager() //StateObject es viejo, no uses esto
+    @State var manager = LocationManager()
     
     var body: some View {
-        Map(coordinateRegion: $manager.region, showsUserLocation: true)
-            .edgesIgnoringSafeArea(.all)
+        //Este Constructor está deprecado
+//        Map(coordinateRegion: $manager.region, showsUserLocation: true)
+        
+        //Este otro constructor es uno de los que no está deprecado, pero hay varios
+        Map(position: $manager.mapCameraPosition, interactionModes: .all)
+        
+//            .edgesIgnoringSafeArea(.all) //No es necesario con el nuevo tipo de mapa, porque ya ingorea toda el area.
     }
 }
 
--- LocationExample/LocationManager.swift
+++ LocationExample/LocationManager.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 import MapKit
+import _MapKit_SwiftUI //Esto seguro tengo q ponerlo solo yo porque debo tener un SDK viejo, pero normalmente todo funciona en con MapKit
 
 final class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
-     var location = CLLocation()
-     var region = MKCoordinateRegion(
+    var location = CLLocation()
+    var region = MKCoordinateRegion(
         center: .init(latitude: 55.334_900, longitude: -122.009_020),
         span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
+    ///Este parametro lo puse simplemente para cumplir con el constructor de map
+    var mapCameraPosition : MapCameraPosition
     
     override init() {
+        mapCameraPosition = .automatic
+//        mapCameraPosition = .region(region) // se puede definir la posicion inicial de varias maneras, con .automatico o con la region que tienes por default, esto se ve principalmente cuando instalas la app por primera vez, porque luego cuando abres la app en otras ocaciones cuando ya está instalada, pues no logras ver la region inicial porque se actualiza muy rapido el location 😂
         super.init()
-        
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.setup()
@@ -27,16 +31,17 @@ final class LocationManager: NSObject, ObservableObject {
     
     func setup() {
         switch locationManager.authorizationStatus {
-        //If we are authorized then we request location just once, to center the map
-        
+            //If we are authorized then we request location just once, to center the map
+            
         case .authorizedWhenInUse, .authorizedAlways:
             locationManager.requestLocation()
             locationManager.startUpdatingLocation()
-        //If we don´t, we request authorization
+            //If we don´t, we request authorization
         case .notDetermined:
-            locationManager.startUpdatingLocation()
+//            locationManager.startUpdatingLocation() //Hay que solicitar autorizacion en este caso, asi que esto no funciona aqui.
             locationManager.requestWhenInUseAuthorization()
-        default:
+            
+        default: // Aqui quizas se pudiera tambien solicitar autorizacion o simplificar el .notDetermined dentro del default.
             break
         }
     }
@@ -45,7 +50,8 @@ final class LocationManager: NSObject, ObservableObject {
 extension LocationManager: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         guard .authorizedWhenInUse == manager.authorizationStatus else { return }
-        locationManager.requestLocation()
+        //Aqui era necesario cambiarlo por esto para que al instalar la app por primera vez comenzara a actualizar la posicion.
+        locationManager.startUpdatingLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -53,20 +59,39 @@ extension LocationManager: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locationManager.startUpdatingLocation()
+        //        locationManager.startUpdatingLocation() //Esto no hace falta llamarlo de nuevo.
         //locationManager.stopUpdatingLocation()
         guard let location = locations.last else { return }
         self.location = location
+        debugPrint("location: \(location)")
         self.region = MKCoordinateRegion(
-                            center: location.coordinate,
-                            span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                        )
-                    }
-//        locations.last.map {
-//            region = MKCoordinateRegion(
-//                center: $0.coordinate,
-//                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-//            )
-//        }
-  //  }
+            center: location.coordinate,
+            span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+//        self.mapCameraPosition = .region(region) // Esta es una variante usando la region
+        
+        //Esta otra usando camera, puedes definirle dos parametros mas a camera, que son heading y pitch
+        let mapCamera = MapCamera(centerCoordinate: location.coordinate, distance: 900)
+//        self.mapCameraPosition = .camera(mapCamera)
+        
+        
+        //Para la posicion del usuario
+        
+        //Aqui se puede jugar con varias configuraciones
+        if mapCameraPosition.positionedByUser {
+            debugPrint("position positionedByUser")
+            self.mapCameraPosition = .userLocation(fallback: .camera(mapCamera))
+            
+        } else { //Esto es un intento de forzar volver a centrar el mapa donde está el usuario , pero no lo tengo muy claro. 
+            debugPrint("position fallback userLocation automatic")
+            self.mapCameraPosition = .userLocation(fallback: .region(region))
+        }
+    }
+    //        locations.last.map {
+    //            region = MKCoordinateRegion(
+    //                center: $0.coordinate,
+    //                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    //            )
+    //        }
+    //  }
 }
